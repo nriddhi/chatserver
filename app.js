84c7d3e1cbf4b657810eddd289a32eb8a20e5cee
Nayeem Hyder Riddhi <riddhi00121@gmail.com>
2023-05-21 16:38:23 +0600

    commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
 const app = express();
+app.use(cookieParser());
 const authRouter = require("./routers/authRouter");
 const contactsRouter = require("./routers/contactsRouter");
 const chatRoomRouter = require("./routers/chatRoomRouter");
@@ -12,28 +12,29 @@ const ReqError = require("./utilities/ReqError");
 const errorController = require("./controllers/errorController");
 
 app.use(express.json({ limit: "50mb" }));
-app.use(cookieParser());
+
 app.use(cors({
   origin: true,
   credentials: true
 }
 ));
 
-app.use(function (req, res, next) {
-  res.header("Content-Type", "application/json;charset=UTF-8");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Content-Type", "application/json;charset=UTF-8");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // Routes
 app.use("/api/user", authRouter);
 
 // Protector
 app.use("/api/*", (req, res, next) => {
+  //console.log(req.cookies.userId);
   if (!req.cookies.userId)
     return next(new ReqError(400, "You are not logged in"));
 
